db86edd2b26d7aba1b318022c4f80a4d5769f4c5
JunYi <nicejunyi@gmail.com>
2025-11-21 15:56:41 +0800

    Change to 3 arrays.

---
+++ 1D_Heat_Transfer_Simulation_in_Air/1D_Heat_Transfer_Simulation_in_Air.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Allocate_memory(float **array1, float **array2, float **array3, int N){
+    *array1 = (float*)malloc(N * sizeof(float));
+    *array2 = (float*)malloc(N * sizeof(float));
+    *array3 = (float*)malloc(N * sizeof(float));
+
+    if (*array1 == NULL || *array2 == NULL || *array3 == NULL){
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    printf("Memory allocation successfully for %d elements\n", N);
+}
+
+void Free_memory( float *array1, float *array2, float *array3){
+        free(array1);
+        free(array2);
+        free(array3);
+        printf("Memory freed successfully\n");
+}
+
+int main(){
+    const int N = 200 ;
+    float *T, *t, *x;
+
+    Allocate_memory(&T, &t, &x, N);
+
+    Free_memory(T, t, x);
+
+    return(0);
+}
