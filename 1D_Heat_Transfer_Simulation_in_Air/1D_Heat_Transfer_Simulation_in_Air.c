2da53030acf117044a40b800c9520d0a16c3ed8f
JunYi <nicejunyi@gmail.com>
2025-11-27 18:22:23 +0800

    Define more parameters.

--- 1D_Heat_Transfer_Simulation_in_Air/1D_Heat_Transfer_Simulation_in_Air.c
+++ 1D_Heat_Transfer_Simulation_in_Air/1D_Heat_Transfer_Simulation_in_Air.c
@@ -21,11 +21,18 @@ void Free_memory( float *array1, float *array2, float *array3){
         printf("Memory freed successfully\n");
 }
 
+//Calculate time when T=325K at x = 5cm.
 int main(){
     const int N = 200;
-    const float alpha = ; 
-    const float dt = ; 
-    const float dx = 0.1/(N-1); // Total length = 0.1 m, and N-1 is because N start from 0.
+    const float k = 0.026; // Thermal conductivity of air is 0.026 W/m K
+    const float R = 287; // Gas constant of air is 287 J/kg K
+    const float rho = 1.23; // density of air is 1.23 kg/m^3
+    const float gamma = 1.4; // Gamma value of air is 1.4
+    const float Cv = R/(gamma-1); // specific heat at constant volume of air
+    const float Cp = Cv+R; // specific heat at constant pressure of air
+    
+    float dt = ; 
+    float dx = 0.1/(N-1); // Total length = 0.1 m, and N-1 is because N start from 0.
 
     float *T, *T_new, *x ;
     float time = 0.0;
