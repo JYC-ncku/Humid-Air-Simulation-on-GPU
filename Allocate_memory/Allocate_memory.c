80bbbeec032d54b0279a6cef56fe647641c2cab7
JunYi <nicejunyi@gmail.com>
2025-11-27 17:38:28 +0800

    Create a folder to save file.

--- Allocate_memory/Allocate_memory.c
+++ Allocate_memory/Allocate_memory.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Allocate_memory(float **array1, float **array2, int N){ //*就是go to address的意思，所以*array1就是go to arryay1 address的意思 **array1就是 address array1 go to address，例如"*我"去"*你家"做一件事情，這件事情只有我可以做，然後完成之後想要清掉我做的事情的痕跡，你不需要特別去知道我在你家做了什麼事情，你就是直接把整個你家所有更新的東西全部清調恢復原狀就好，所以只需要Free(*)，不需要Free(**)。
+void Allocate_memory(float **array1, float **array2, int N){ //*就是go to address的意思，所以*array1就是go to arryay1 address的意思 **array1就是 address array1 go to address，
+                                                             //例如"*我"去"*你家"做一件事情，這件事情只有我可以做，然後完成之後想要清掉我做的事情的痕跡，你不需要特別去知道我在你家做了什麼事情，
+                                                             //你就是直接把整個你家所有更新的東西全部清調恢復原狀就好，所以只需要Free(*)，不需要Free(**)。
     *array1 = (float*)malloc(N * sizeof(float));
     *array2 = (float*)malloc(N * sizeof(float));
 
