d3a0397234d386cde705f201391608362a1a3ee8
JunYi <nicejunyi@gmail.com>
2025-12-09 17:59:39 +0800

    Add main function to calculate y=sin(x).

--- sine_wave/sine_wave.c
+++ sine_wave/sine_wave.c
@@ -10,7 +10,7 @@ void Allocate_memory(float **array1, float **array2, int N){
         printf("Memory allocation faild!\n");
         exit(1);
     }
-        printf("Memory allocation successfully for %d elements\n", N)
+        printf("Memory allocation successfully for %d elements\n", N);
 }
 
 void Free_memory(float *array1, float *array2){
@@ -19,3 +19,15 @@ void Free_memory(float *array1, float *array2){
     printf("Memory freed successfully!\n");
 }
 
+int main(){
+    const int N = 200;
+    float *x, *y;
+
+    Allocate_memory(&x, &y, N);
+    for(int i=0; i<N; i++){
+        x[i] = 5.0f * i / (N-1); 
+        y[i] = sinf(x[i]);
+    }
+    Free_memory(x, y);
+    return 0;
+}
