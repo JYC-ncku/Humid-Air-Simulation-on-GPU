7717967f09cc87d87afd3fc41c8916185ece14b8
JunYi <nicejunyi@gmail.com>
2025-12-09 13:57:00 +0800

    add result.txt

--- 1D_Heat_Transfer_Simulation_in_Air/1D_Heat_Transfer_Simulation_in_Air.c
+++ 1D_Heat_Transfer_Simulation_in_Air/1D_Heat_Transfer_Simulation_in_Air.c
@@ -39,6 +39,7 @@ void Free_memory( float *array1, float *array2, float *array3){
 
 // Using FTCS to calculate time when T=325K at x = 5cm.
 void FTCS_1D(int N, int no_steps){
+    FILE *pFile;
     // 變數宣告
     float *x, *T, *T_new;      
     // malloc x, T, T_new
@@ -52,6 +53,14 @@ void FTCS_1D(int N, int no_steps){
     float dt = 0.001; // 固定時間步長(Time step)，若r值檢測大於0.5則須將此值調小。有兩種方式：一是固定r值，另外就是固定dt，由於這題我們想得到的是時間，所以固定dt所得到的答案較精確。
     // 3. 穩定參數 R (PHI) 計算
     float PHI = alpha * dt / (dx * dx); // PHI = alpha*(dt/dx^2)
+    float time_to_reach = 0.0; //initialize the time,為了計算達到目標的時間，必須先將時間初始化。
+    int target_reached = 0; //target_reached是一個旗標 (Flag) 變數，它用來表示某個布林（Boolean）狀態：目標是否達成，0表示未達成(not completed)、1表示已達成(complete)。
+    float Cell_heat_flux;
+
+    // Open file
+    pFile = fopen("results.txt","w");
+
+
     // 4. 穩定性檢查(stability analysis) (PHI < 0.5) 
     if (PHI >= 0.5) {
             printf("ERROR: PHI must be < 0.5, your PHI is %.3f.\n", PHI);
@@ -60,8 +69,6 @@ void FTCS_1D(int N, int no_steps){
             return ; // 會退出當前函式，回到呼叫處。
     }
     // 5. 初始條件設定(Setting initial condition),t=0       
-    float time_to_reach = 0.0; //initialize the time,為了計算達到目標的時間，必須先將時間初始化。
-    int target_reached = 0; //target_reached是一個旗標 (Flag) 變數，它用來表示某個布林（Boolean）狀態：目標是否達成，0表示未達成(not completed)、1表示已達成(complete)。
     for(int i=0; i < N; i++){
         x[i] = i * dx;
         T[i] = Ti; 
@@ -99,7 +106,7 @@ void FTCS_1D(int N, int no_steps){
     
     // calculate heat flux.
     
-    float Cell_heat_flux = -((T[j] - T[j+1]) / dx) * k;  
+    Cell_heat_flux = -((T[j] - T[j+1]) / dx) * k;  
     printf("Cell %d : heat_flux = %.3f W\n", j, Cell_heat_flux);
     }
 
@@ -109,6 +116,11 @@ void FTCS_1D(int N, int no_steps){
     } else {
         printf("Target achieved! The time required for the temperature at x = 5 cm to reach 325 K is %.3f seconds.\n", time_to_reach);
     }
+
+
+
+    fclose(pFile);
+
     Free_memory(T, T_new, x); 
 }
 // Setting the number of grid points.
