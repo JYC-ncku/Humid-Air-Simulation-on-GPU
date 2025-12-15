94a20b63174e20c7d2bc19d0298a56c6da50d713
JunYi <nicejunyi@gmail.com>
2025-12-15 18:37:14 +0800

    Define initial and boundary condition.

--- Perform_1D_humidity_simulation_in_Air/Perform_1D_humidity_simulation_in_Air.c
+++ Perform_1D_humidity_simulation_in_Air/Perform_1D_humidity_simulation_in_Air.c
@@ -8,6 +8,8 @@ const float hum_init = 0;
 const float hum_final = 1;
 const float hum_target = 0.25;
 
+//Set a maximum number of steps
+const int no_steps = 10000;
 
 void Allocate_memory(float **array1, float **array2, float **array3, int N){
     *array1 = (float*)malloc(N * sizeof(float));
@@ -27,7 +29,34 @@ void Free_memory(float *array1, float *array2, float *array3){
     printf("Memory freed successfully!\n");
 }
 
+void Humidity_1D(int N, no_steps){
+    float D = 2.0e-5; //Diffusivity of air.
+    float *x, *hum, *hum_new;
+    float dx = L / (N-1);
+    float PHI = 0.25;
+    float dt = (PHI * (dx*dx))/D;
+    float time_to_reach = 0; //Iintialize the time.
+    int target_reach = 0;
+
+    Allocate_memory(&x, &hum, &hum_new, N);
+        for (int i=0; i<N; i++){
+            x[i]=i * dx;
+            hum[i]=hum_init;
+            hum_new[i]=hum_init;
+        }
+        hum[N-1]=hum_final;
+        hum_new[N-1]=hum_final;
+
+        time_to_reach += dt;
+        
+
+
+
+    Free_memory(x, hum, hum_new);
+}
+
 int main(){
-    int N=200;
+    const int N=200;
+    Humidity_1D(N, no_steps);
     return(0);
 }
