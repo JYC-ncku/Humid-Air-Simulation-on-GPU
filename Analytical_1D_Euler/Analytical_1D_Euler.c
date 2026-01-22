76b5630d40b80d62c4c57f571be7faa7ffaa12ba
JunYi <nicejunyi@gmail.com>
2026-01-22 10:54:58 +0800

    Add dx and dt.

--- Analytical_1D_Euler/Analytical_1D_Euler.c
+++ Analytical_1D_Euler/Analytical_1D_Euler.c
@@ -2,29 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-void Allocate_memory(double **array1, double **array2, double **array3, double **array4, double **array5, int N_CELLS){
+void Allocate_memory(double **array1, double **array2, double **array3, int N_CELLS){
     *array1 = (double*)malloc(N_CELLS * sizeof(double));
     *array2 = (double*)malloc(N_CELLS * sizeof(double));
     *array3 = (double*)malloc(N_CELLS * sizeof(double));
-    *array4 = (double*)malloc(N_CELLS * sizeof(double));
-    *array5 = (double*)malloc(N_CELLS * sizeof(double));
-    if(*array1 == NULL || *array2 == NULL || *array3 == NULL || *array4 == NULL || *array5 == NULL){
+    if(*array1 == NULL || *array2 == NULL || *array3 == NULL){
         printf("Memory allocation failed!\n");
     }
         printf("Memory allocation successfully for %d elements!\n", N_CELLS);
 }
 
-void Free_memory(double *array1, double *array2, double *array3, double *array4, double *array5){
+void Free_memory(double *array1, double *array2, double *array3){
     free(array1);
     free(array2);
     free(array3);
-    free(array4);
-    free(array5);
     printf("Memory freed successfully!\n");
 }
 
 // Return the maximum CFL number across all cells
-double CPU_Compute_CFL(double *p0, double *p1, double *p2, double DX, double DT, int N_CELLS){
+double CPU_Compute_MAX_CFL(double *p0, double *p1, double *p2, float dx, float dt, int N_CELLS){
     float MAX_CFL = -1.0;
     for (int cell=0; cell<N_CELLS; cell++){
         double rho = p0[cell];
@@ -35,7 +31,7 @@ double CPU_Compute_CFL(double *p0, double *p1, double *p2, double DX, double DT,
             exit(1);
         }
         double a = sqrt(1.4 * 1.0 *T); // gamma = 1.4, R = 1.0
-        double CFL = (fabs(u) + a) * (DT / DX);
+        double CFL = (fabs(u) + a) * (dt / dx);
         if (CFL>MAX_CFL){
             MAX_CFL = CFL;
         }
@@ -51,7 +47,10 @@ double CPU_Compute_CFL(double *p0, double *p1, double *p2, double DX, double DT,
 
 int main(){
     int N_CELLS = 200;
-    double *p0, *p1, *p2, *DX, *DT;
-    Allocate_memory(&p0, &p1, &p2, &DX, &DT, N_CELLS);
-    Free_memory(p0, p1, p2, DX, DT);
+    double *p0, *p1, *p2; 
+    float dx = 1/N_CELLS; 
+    float dt = 0.0001; //隨便設，如果CFL值出錯就調整dt值。
+    Allocate_memory(&p0, &p1, &p2, N_CELLS);
+    CPU_Compute_MAX_CFL(p0, p1, p2, dx, dt, N_CELLS);
+    Free_memory(p0, p1, p2);
 }
