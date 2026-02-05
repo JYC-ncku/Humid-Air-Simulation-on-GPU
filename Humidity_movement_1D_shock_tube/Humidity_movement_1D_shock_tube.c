3297fcbfe771403555569b393e2f5b75cbe7e8ca
JunYi <nicejunyi@gmail.com>
2026-02-05 16:50:43 +0800

    Plots for density, velocity, temperature, pressure, humidity vs  position, with a grid size of 200."

--- Humidity_movement_1D_shock_tube/Humidity_movement_1D_shock_tube.c
+++ Humidity_movement_1D_shock_tube/Humidity_movement_1D_shock_tube.c
@@ -79,13 +79,15 @@ double MAX_Wave_Speed(double u_L, double u_R, double a_L, double a_R){
 }
 
 void Calc_Rusanov_Flux(double rho_L, double rho_R, double u_L, double u_R, double T_L, double T_R, double p_L, double p_R, double e_L, double e_R, double W_LOCAL_MAX,
-                       double *mass_flux, double *momentum_flux, double *energy_flux, double *rhov_flux, double rhov_L, double rhov_R, double H_L, double H_R, int j){
+                       double *mass_flux, double *momentum_flux, double *energy_flux, double *rhov_flux, double H_L, double H_R, int j){
     double mass_L = rho_L;
     double momentum_L = rho_L * u_L;
     double energy_L = e_L;
     double mass_R = rho_R;
     double momentum_R = rho_R * u_R;
     double energy_R = e_R;
+    double rhov_L = H_L * rho_L; // phov is vapor density = rho * humidity
+    double rhov_R = H_R * rho_R;
 
     double mass_flux_L = rho_L * u_L;
     double mass_flux_R = rho_R * u_R;
@@ -93,8 +95,8 @@ void Calc_Rusanov_Flux(double rho_L, double rho_R, double u_L, double u_R, doubl
     double momentum_flux_R = rho_R * u_R * u_R + p_R;
     double energy_flux_L = (e_L + p_L) * u_L;
     double energy_flux_R = (e_R + p_R) * u_R;
-    double rhov_flux_L = rho_L * H_L;
-    double rhov_flux_R = rho_R * H_R;
+    double rhov_flux_L = rhov_L * u_L;
+    double rhov_flux_R = rhov_R * u_R;
 
     mass_flux[j] = 0.5 * (mass_flux_L + mass_flux_R) - 0.5 * W_LOCAL_MAX * (mass_R - mass_L);
     momentum_flux[j] = 0.5 * (momentum_flux_L + momentum_flux_R) - 0.5 * W_LOCAL_MAX * (momentum_R - momentum_L);
@@ -130,31 +132,30 @@ int main(){
             p0[i] = 10;
             p1[i] = 0;
             p2[i] = 1;
-            p3[i] = 10;
             p4[i] = 1;
         } else {
             p0[i] = 1;
             p1[i] = 0;
             p2[i] = 1;
-            p3[i] = 1;
             p4[i] = 0;
         }
     }
+
+    for (int i = 0; i<N_CELLS; i++){
+        R[i] = ((1 - p4[i]) * (R_dry / R_dry) + p4[i] * (R_v / R_dry)); // 所有參數都(密度、速度、溫度、壓力)都是用無因次化去做計算，所以R跟CV也要無因次化，通常以dry air為基準。
+        CV[i] = (1 - p4[i]) * (CV_dry / CV_dry) + p4[i] * (CV_v / CV_dry);
+        GAMMA[i] = 1 + (R[i] / CV[i]);
+        p3[i] = p0[i] * R[i] * p2[i];
+    }    
 
     for (int i=0; i<N_CELLS; i++){
         mass[i] = p0[i];
         momentum[i] = p0[i] * p1[i];
         energy[i] = 0.5 * p0[i] * p1[i] * p1[i] + (p3[i] / (GAMMA[i] - 1));
+        rhov[i] = p0[i] * p4[i];
     }
 
    while (t<t_FINAL){
-        for (int i = 0; i<N_CELLS; i++){
-            R[i] = (1 - p4[i]) * R_dry + p4[i] * R_v;
-            CV[i] = (1 - p4[i]) * CV_dry + p4[i] * CV_v;
-            GAMMA[i] = 1 + (R[i] / CV[i]);
-            p3[i] = p0[i] * R[i] * p2[i];
-            a[i] = sqrt(GAMMA[i] * R[i] * p2[i]);
-        }
 
         // In order to compute dt, need to find the Max wave speed first.
         double W_GLOBAL_MAX = 1.0e-10;
@@ -162,8 +163,6 @@ int main(){
         for (int j = 1; j < N_CELLS; j++){
 			double rho_L = p0[j-1];
     		double rho_R = p0[j];
-            double rhov_L = p4[j-1] * rho_L; // phov is vapor density = rho * humidity
-            double rhov_R = p4[j] * rho_R;
     		double u_L = p1[j-1];
     		double u_R = p1[j];
     		double T_L = p2[j-1];
@@ -172,17 +171,17 @@ int main(){
             double p_R = p3[j];
             double H_L = p4[j-1];
             double H_R = p4[j];
-            double e_L = 0.5 * rho_L * u_L * u_L + (p_L / (GAMMA[j-1] - 1));
-            double e_R = 0.5 * rho_R * u_R * u_R + (p_R / (GAMMA[j-1] - 1));
             double GAMMA_L = GAMMA[j-1];
             double GAMMA_R = GAMMA[j];
+            double e_L = 0.5 * rho_L * u_L * u_L + (p_L / (GAMMA_L - 1));
+            double e_R = 0.5 * rho_R * u_R * u_R + (p_R / (GAMMA_R - 1));
             double R_L = R[j-1];
             double R_R = R[j];
             double a_L = sqrt(GAMMA_L * R_L * T_L);
             double a_R = sqrt(GAMMA_R * R_R * T_R);
             double W_LOCAL_MAX = MAX_Wave_Speed(u_L, u_R, a_L, a_R);
             Calc_Rusanov_Flux(rho_L, rho_R, u_L, u_R, T_L, T_R, p_L, p_R, e_L, e_R, W_LOCAL_MAX,
-                              mass_flux, momentum_flux, energy_flux, rhov_flux, rhov_L, rhov_R, H_L, H_R, j);
+                              mass_flux, momentum_flux, energy_flux, rhov_flux,  H_L, H_R, j);
             if (W_LOCAL_MAX > W_GLOBAL_MAX){
                 W_GLOBAL_MAX = W_LOCAL_MAX;
             }
@@ -194,9 +193,11 @@ int main(){
         mass_flux[0] = mass_flux[1];
         momentum_flux[0] = momentum_flux[1];
         energy_flux[0] = energy_flux[1];
+        rhov_flux[0] = rhov_flux[1];
         mass_flux[N_CELLS] = mass_flux[N_CELLS - 1];
         momentum_flux[N_CELLS] = momentum_flux[N_CELLS - 1];
         energy_flux[N_CELLS] = energy_flux[N_CELLS - 1];
+        rhov_flux[N_CELLS] = rhov_flux[N_CELLS - 1];
     
         // Use FVM to get new conservation values
         for (int i=0; i<N_CELLS; i++){
@@ -209,9 +210,9 @@ int main(){
         for (int i = 0; i<N_CELLS; i++){
             p0[i] = mass[i];
             p1[i] = momentum[i] / mass[i];
+            p4[i] = rhov[i]/p0[i];
             p3[i] = (GAMMA[i] - 1) * (energy[i] - 0.5 * p0[i] * p1[i] * p1[i]);
             p2[i] = p3[i] / (p0[i] * R[i]);
-            p4[i] = rhov[i]/p0[i];
         }
 
         t += dt;        
@@ -219,7 +220,7 @@ int main(){
 
     FILE * pFile = fopen("Results_of_200_cells.txt","w");
     for (int i = 0; i<N_CELLS; i++){
-        fprintf(pFile, "%.3f\t%.6f\t%.6f\t%.6f\t%.6f\n", x[i], p0[i], p1[i], p2[i], p3[i]);
+        fprintf(pFile, "%.3f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n", x[i], p0[i], p1[i], p2[i], p3[i], p4[i]);
     }
     fclose(pFile);
 
