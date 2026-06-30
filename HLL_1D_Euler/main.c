6386c8c4d499badba1ff8a49dfe1bb2361b2299d
JunYi <nicejunyi@gmail.com>
2026-06-30 12:19:45 +0800

    Add allocate and free memory function.

--- HLL_1D_Euler/main.c
+++ HLL_1D_Euler/main.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "memory.h"
 
 int main(){
+	int N_CELLS = 200;
+	float *x, *p0, *p1, *p2, *p3, *mass, *momentum, *energy, *mass_flux, *momentum_flux, *energy_flux;
+	float L = 1.0;
+	float t = 0;
+	float t_FINAL = 0.2;
+	float R = 1.0;
+	float GAMMA = 1.4;
+	float CFL = 0.25;
+	float dx = L/N_CELLS;
+	float W_GLOBAL_MAX;
+	Allocate_memory(&x, &p0, &p1, &p2, &p3, &mass, &momentum, &energy, &mass_flux, &momentum_flux, &energy_flux, N_CELLS);
 	printf("Hello world\n");
+	Free_memory(&x, &p0, &p1, &p2, &p3, &mass, &momentum, &energy, &mass_flux, &momentum_flux, &energy_flux);
 return 0;
 }
 
