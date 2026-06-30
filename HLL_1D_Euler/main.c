b961f7bd6f3a35a514e02fb07fa74e8f77699b24
JunYi <nicejunyi@gmail.com>
2026-06-30 14:25:49 +0800

    Setting initial conditions.

--- HLL_1D_Euler/main.c
+++ HLL_1D_Euler/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "memory.h"
+#include "Initial.h"
 
 int main(){
 	int N_CELLS = 200;
@@ -14,6 +15,10 @@ int main(){
 	float dx = L/N_CELLS;
 	float W_GLOBAL_MAX;
 	Allocate_memory(&x, &p0, &p1, &p2, &p3, &mass, &momentum, &energy, &mass_flux, &momentum_flux, &energy_flux, N_CELLS);
+	Initial(x, p0, p1, p2, p3, mass, momentum, energy, dx, GAMMA, N_CELLS);
+	for (int i = 0; i < N_CELLS; i++){
+		printf("p0[%d] = %g\n", i, p0[i]);
+	}
 	printf("Hello world\n");
 	Free_memory(&x, &p0, &p1, &p2, &p3, &mass, &momentum, &energy, &mass_flux, &momentum_flux, &energy_flux);
 return 0;
