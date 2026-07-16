7bb5680012f479ae9bc0458412b7074aedefbf95
JunYi <nicejunyi@gmail.com>
2026-07-16 14:27:39 +0800

    Add flux_Y and modoify boundary condition.

--- Exact_Riemann_Solver_2D_Euler/main.c
+++ Exact_Riemann_Solver_2D_Euler/main.c
@@ -4,10 +4,20 @@
 #include "memory.h"
 #include "Calc_rho_u_P_T.h"
 
+/*
+	  GHOST				  GHOST
+	一一一一一一一一一一一一一一一一一一一一
+	|	|	|       |	|	|
+	|   0   |   1   |  ...  |   N   |  N+1  |
+	|       |       |       |	|	|
+	一一一一一一一一一一一一一一一一一一一一
+		0	1      ...	N
+*/
+
 int main(){
 	int NX = 300;
 	int NY = 100;
-	int N_CELLS = NX * NY;
+	int N_CELLS = (NX+2) * (NY+2); //+2 is for Ghost cells
 	float L = 3.0;
 	float H = 1.0;
 	float dx = L/NX;
@@ -18,10 +28,10 @@ int main(){
 	float R = 1.0;
 	float GAMMA = 1.4;
 	int wall_flag = 0;
-	float *x, *p0, *p1, *p2, *p3, *p4, *interface_p, *flux; //p0 is density, p1 is x-dir velocity, p2 is y-dir veloctiy, p3 is temperature, p4 si pressure.
+	float *x, *p0, *p1, *p2, *p3, *p4, *interface_p, *flux_X, *flux_Y; //p0 is density, p1 is x-dir velocity, p2 is y-dir veloctiy, p3 is temperature, p4 si pressure.
 	float flxnmn, flxpmn, flxqmn;
 
-	Allocate_memory(&x, &p0, &p1, &p2, &p3, &p4, &interface_p, &flux, N_CELLS);
+	Allocate_memory(&x, &p0, &p1, &p2, &p3, &p4, &interface_p, &flux_X, &flux_Y, N_CELLS);
 	//Initial condition
 	for ( int i = 0; i<N_CELLS; i++){
 		x[i] = (i+0.5) * dx;
@@ -38,75 +48,149 @@ int main(){
 		}
 		p4[i] = p0[i] * R * p3[i];
 	}
+/* For x-dir
 	// Because this code only consider 1D, so let other two direction equal 0)
 	float QL_vy = 1.0, QL_vz = 0;
 	float QR_vy = 1.0, QR_vz = 0;
 	float nx = 1.0, ny = 0.0, nz = 0.0;
 	float px = 0.0, py = 1.0, pz = 0.0;
 	float qx = 0.0, qy = 0.0, qz = 1.0;
+*/
 
+/* For y-dir
+	// Because this code only consider 1D, so let other two direction equal 0)
+	float QL_vy = 1.0, QL_vz = 0;
+	float QR_vy = 1.0, QR_vz = 0;
+	float nx = 0.0, ny = 1.0, nz = 0.0;
+	float px = -1.0, py = 0.0, pz = 0.0;
+	float qx = 0.0, qy = 0.0, qz = 1.0;
+*/
 	while (t<t_FINAL){
-    	float MAX_CFL = CPU_Compute_MAX_CFL(p0, p1, p2, p3, dx, dy, dt, N_CELLS);
-
-		for (int i = 0; i < N_CELLS-1; i++){
-			float QL_rho = p0[i];
-	    		float QR_rho = p0[i+1];
-    			float QL_ux  = p1[i];
-    			float QR_ux  = p1[i+1];
-	    		float QL_T   = p2[i];
-    			float QR_T   = p2[i+1];
-			float QL_cRT = sqrt(R * QL_T);
-    			float QR_cRT = sqrt(R  * QR_T);
-			CPU_Calc_rho_u_P_T(&interface_p[i*6], &flux[i*5], //因為flux跟interface_p都有5個物理量需要儲存，如果不加這行的話數據就會一直不斷被覆蓋，最後變成只有儲存到最後一格的資料。
-					   QL_rho, QL_ux, QL_vy, QL_vz, QL_cRT,
-					   QR_rho, QR_ux, QR_vy, QR_vz, QR_cRT, R, GAMMA,
-					   nx, ny, nz,
-					   px, py, pz,
-					   qx, qy, qz, wall_flag);
+	    	float MAX_CFL = CPU_Compute_MAX_CFL(p0, p1, p2, p3, dx, dy, dt, N_CELLS);
+	    	//X-dir (flux_X)
+		for (int i = 0; i < NX + 1; i++){		//200 cells have 201 interface
+			for (int j = 0; j < NY + 1; j++){
+				int INDEX = i * NY + j;
+				float QL_rho = p0[INDEX];
+		    		float QR_rho = p0[INDEX+1];
+    				float QL_ux = p1[INDEX];
+    				float QR_ux = p1[INDEX+1];
+    				float QL_vy = p2[INDEX];
+    				float QR_vy = p2[INDEX+1];
+    				float QL_vz  = 0.0;
+    				float QR_vz  = 0.0;
+		    		float QL_T = p3[INDEX];
+    				float QR_T = p3[INDEX+1];
+				float QL_cRT = sqrt(R * QL_T);
+    				float QR_cRT = sqrt(R * QR_T);
+				CPU_Calc_rho_u_P_T(&interface_p[INDEX*6], &flux_X[INDEX*5], //因為flux跟interface_p都有5個物理量需要儲存，如果不加這行的話數據就會一直不斷被覆蓋，最後變成只有儲存到最後一格的資料。
+						   QL_rho, QL_ux, QL_vy, QL_vz, QL_cRT,
+						   QR_rho, QR_ux, QR_vy, QR_vz, QR_cRT, R, GAMMA,
+						   1.0, 0.0, 0.0,
+						   0.0, 1.0, 0.0,
+						   0.0, 0.0, 1.0, wall_flag);
+			}
 		}
 
-		for (int i = 1; i < N_CELLS - 1; i++) {
-    		// 我們是 i*5，所以左界面是 (i-1)*5，右界面是 i*5
-			int L_interface = (i - 1) * 5;
-			int R_interface = i * 5;
-			float CV = R / (GAMMA - 1.0);
-			// 先將舊的值儲存起來
-			float rho_old = p0[i];
-			float u_old   = p1[i];
-			float T_old   = p2[i];
-			// p1 存的是速度 u，我們要先算動量 rho*u 的變化再去除以rho得到u。
-			float Mom_old = rho_old * u_old; // p0[i] * p1[i]
-			// 先從溫度算總能 E，更新完 E 再扣掉動能回算 T
-			float E_old   = rho_old * (CV * T_old + 0.5 * u_old * u_old); //p0[i] * (CV * p2[i] + 0.5 * p1[i] * p1[i])
-			// 使用FVM計算新的值，參考程式碼712～716行，interface_p[0]是密度、[1]是u、[2]是v、[3]是w、[4]是溫度。
-			float rho_new = rho_old + (dt / dx) * (flux[L_interface + 0] - flux[R_interface + 0]);
-			float Mom_new = Mom_old + (dt / dx) * (flux[L_interface + 1] - flux[R_interface + 1]);
-			float E_new   = E_old   + (dt / dx) * (flux[L_interface + 4] - flux[R_interface + 4]);
-			//更新密度 (p0)
-	    		p0[i] = rho_new;
-    			// 更新動量並回推速度 (p1)
-    			p1[i] = Mom_new / rho_new;
-	    		// 更新能量並回推溫度 (p2)
-			float internal_e = (E_new / rho_new) - 0.5 * (p1[i] * p1[i]);
-			p2[i] = internal_e / CV;
-			// 更新壓力 (p3)
-			float P_new = p0[i] * R * p2[i];
-			p3[i] = P_new;
+		//Y-dir (flux_Y) 把X軸往逆時針轉90度看。
+		for (int i = 0; i < NX + 1; i++){		//200 cells have 201 interface
+			for (int j = 0; j < NY + 1; j++){
+				int INDEX = i * NY + j;
+				float QL_rho = p0[INDEX];
+		    		float QR_rho = p0[INDEX+1];
+    				float QL_ux  = p1[INDEX];
+    				float QR_ux  = p1[INDEX+1];
+    				float QL_vy = p2[INDEX];
+    				float QR_vy = p2[INDEX+1];
+    				float QL_vz  = 0.0;
+    				float QR_vz  = 0.0;
+		    		float QL_T   = p3[INDEX];
+    				float QR_T   = p3[INDEX+1];
+				float QL_cRT = sqrt(R * QL_T);
+    				float QR_cRT = sqrt(R * QR_T);
+				CPU_Calc_rho_u_P_T(&interface_p[INDEX*6], &flux_Y[INDEX*5], //因為flux跟interface_p都有5個物理量需要儲存，如果不加這行的話數據就會一直不斷被覆蓋，最後變成只有儲存到最後一格的資料。
+						   QL_rho, QL_ux, QL_vy, QL_vz, QL_cRT,
+						   QR_rho, QR_ux, QR_vy, QR_vz, QR_cRT, R, GAMMA,
+						   0.0, 1.0, 0.0,
+						   -1.0, 0.0, 0.0,
+						   0.0, 0.0, 1.0, wall_flag);
+			}
+		}
+
+		for (int i = 1; i < NX + 1; i++){		//200 cells, the ghost cells on the left and right are not include in calculation.
+			for (int j = 1; j < NY + 1; j++){
+				int INDEX = i * NY + j;
+	    			// 我們是 i*5，所以左界面是 (i-1)*5，右界面是 i*5
+				int L_interface = (INDEX - 1) * 5;
+				int R_interface = INDEX * 5;
+				float CV = R / (GAMMA - 1.0);
+				// 先將舊的值儲存起來
+				float rho_old = p0[INDEX];
+				float u_old   = p1[INDEX];
+				float v_old   = p2[INDEX];
+				float T_old   = p3[INDEX];
+				// p1 存的是速度 u，我們要先算動量 rho*u 的變化再去除以rho得到u。
+				float MomX_old = rho_old * u_old; // p0[INDEX] * p1[INDEX]
+				float MomY_old = rho_old * v_old; // p0[INDEX] * p1[INDEX]
+				// 先從溫度算總能 E，更新完 E 再扣掉動能回算 T
+				float E_old = rho_old * (CV * T_old + 0.5 * (u_old * u_old + v_old * v_old)); //p0[INDEX] * (CV * p2[INDEX] + 0.5 * (p1[INDEX] * p1[INDEX] + p2[INDEX] * p2[INDEX]))
+				// 使用FVM計算新的值，interface_p[0]是密度、[1]是u、[2]是v、[3]是w、[4]是溫度。
+				float rho_new = rho_old + (dt / dx) * (flux_X[L_interface + 0] - flux_X[R_interface + 0])
+							+ (dt / dy) * (flux_Y[L_interface + 0] - flux_Y[R_interface + 0]);
+				float MomX_new = MomX_old + (dt / dx) * (flux_X[L_interface + 1] - flux_X[R_interface + 1])
+							 + (dt / dy) * (flux_Y[L_interface + 1] - flux_Y[R_interface + 1]);
+				float MomY_new = MomY_old + (dt / dx) * (flux_X[L_interface + 2] - flux_X[R_interface + 2])
+							 + (dt / dy) * (flux_Y[L_interface + 2] - flux_Y[R_interface + 2]);
+				float E_new = E_old + (dt / dx) * (flux_X[L_interface + 4] - flux_X[R_interface + 4])
+						    + (dt / dy) * (flux_Y[L_interface + 4] - flux_Y[R_interface + 4]);
+				//更新密度 (p0)
+		    		p0[INDEX] = rho_new;
+    				// 更新動量並回推速度 (p1, p2)
+    				p1[INDEX] = MomX_new / rho_new;
+    				p2[INDEX] = MomY_new / rho_new;
+		    		// 更新能量並回推溫度 (p3)
+				float internal_e = (E_new / rho_new) - 0.5 * (p1[INDEX] * p1[INDEX] + p2[INDEX] * p2[INDEX]);
+				p3[INDEX] = internal_e / CV;
+				// 更新壓力 (p4)
+				p4[INDEX] = p0[INDEX] * R * p3[INDEX];
+			}
 		}
 
 		// Boundary condition for compute flux.
-		// 左邊界
-		p0[0] = p0[1];
-		p1[0] = p1[1];
-		p2[0] = p2[1];
-		p3[0] = p3[1];
-
-		// 右邊界
-		p0[N_CELLS-1] = p0[N_CELLS-2];
-		p1[N_CELLS-1] = p1[N_CELLS-2];
-		p2[N_CELLS-1] = p2[N_CELLS-2];
-		p3[N_CELLS-1] = p3[N_CELLS-2];
-
+		// LEFT and RIGHT
+		for (int j = 1 ; j <= NY; j++){
+			int LEFT_GHOST = 0 * (NY+2) + j;
+			int RIGHT_GHOST = (NX+1) * (NY+2) + j;
+			int LEFT_INNER = 1 * (NY+2) + j;
+			int RIGHT_INNER = NX * (NY+2) + j;
+			p0[LEFT_GHOST] = p0[LEFT_INNER];
+			p0[RIGHT_GHOST] = p0[RIGHT_INNER];
+			p1[LEFT_GHOST] = p1[LEFT_INNER];
+			p1[RIGHT_GHOST] = p1[RIGHT_INNER];
+			p2[LEFT_GHOST] = p2[LEFT_INNER];
+			p2[RIGHT_GHOST] = p2[RIGHT_INNER];
+			p3[LEFT_GHOST] = p3[LEFT_INNER];
+			p3[RIGHT_GHOST] = p3[RIGHT_INNER];
+			p4[LEFT_GHOST] = p4[LEFT_INNER];
+			p4[RIGHT_GHOST] = p4[RIGHT_INNER];
+		}
+		//BOTTOM and TOP
+		for (int i = 1 ; i <= NX; i++){
+			int BOTTOM_GHOST = i * (NY+2) + 0;
+			int TOP_GHOST = i * (NY+2) + (NY+1);
+			int BOTTOM_INNER = i * (NY+2) + 1;
+			int TOP_INNER = i * (NY+2) + NY;
+			p0[BOTTOM_GHOST] = p0[BOTTOM_INNER];
+			p0[TOP_GHOST] = p0[TOP_INNER];
+			p1[BOTTOM_GHOST] = p1[BOTTOM_INNER];
+			p1[TOP_GHOST] = p1[TOP_INNER];
+			p2[BOTTOM_GHOST] = p2[BOTTOM_INNER];
+			p2[TOP_GHOST] = p2[TOP_INNER];
+			p3[BOTTOM_GHOST] = p3[BOTTOM_INNER];
+			p3[TOP_GHOST] = p3[TOP_INNER];
+			p4[BOTTOM_GHOST] = p4[BOTTOM_INNER];
+			p4[TOP_GHOST] = p4[TOP_INNER];
+		}
 		t += dt;
 	}
 	FILE * pFile = fopen("Results_of_200_cells.txt","w");
@@ -115,6 +199,6 @@ int main(){
 	}
 	fclose(pFile);
 
-	Free_memory(&x, &p0, &p1, &p2, &p3, &p4, &interface_p, &flux);
+	Free_memory(&x, &p0, &p1, &p2, &p3, &p4, &interface_p, &flux_X, &flux_Y);
 return 0;
 }
