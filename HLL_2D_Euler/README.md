f4f96ad91494cd29f96274c89380b470dcd4e4d1
JunYi <nicejunyi@gmail.com>
2026-08-12 14:47:33 +0800

    Change to copy the 2D_Rusanov program to here, I plan to modify the 2D Rusanov program to HLL.

--- HLL_2D_Euler/README.md
+++ HLL_2D_Euler/README.md
@@ -1,37 +1,47 @@
-The purpose of this program is to simulate a 1D Euler Equation problem using the "HLL flux" method and compare the final results with those obtained from the Rusanov flux method.
+This program is a Finite Volume Method (FVM) solver for 2D Euler equations. 
+
+This program extends the 1D domain to a 2D domain, still solving it with an Rusanov solver.
+To validate the results, I ran two test cases.
+Theoretically, the two results must be identical, and the final simulated data matched our expectations perfectly.
 
 Initial condition:
 ```
-u = velocity = 0 m/s (everywhere),   T = temperature = 1 K (everywhere)
-Density = 10m  (x < 0.5L)
-	       1m  (x >= 0.5L)
-Ratio of specific heats = 1.4, R = 1, L = 1m. Computed time = 0.2s.
+Case1:
+u = X-direction velocity = 0 m/s (everywhere), v = Y-direction veloctiy = 0m/s (everywhere), T = temperature = 1 K (everywhere)
 
+Density = 10m  (x <= 0.5L)
+           1m  (x > 0.5L)
+
+Case2:
+u = X-direction velocity = 0 m/s (everywhere), v = Y-direction veloctiy = 0m/s (everywhere), T = temperature = 1 K (everywhere)
+
+Density = 10m  (y <= 0.5L)
+           1m  (y > 0.5L)
+
+Ratio of specific heats = 1.4, R = 1, L = 1m. Computed time = 0.2s.
 ```
 
 compile this code using :
 ```
-gcc main.c memory.c Initial.c Calc_flux.c -O3 -o main.exe -lm
+COMPLIER := gcc
+OPT_FLAGS := -O3 -lm
+all:
+	${COMPLIER} main.c memory.c Calc_rho_u_P_T.c Boundary.c ${OPT_FLAGS} -o main.exe
 ```
 
-Final results for each number of cells (200, 400, 800).
+Final results for 2 cases:
 
-Density of each cells:
-![HLL_vs_Rusanov_for_200_cells_of_Density.png](./HLL_vs_Rusanov_for_200_cells_of_Density.png)
-![HLL_vs_Rusanov_for_400_cells_of_Density.png](./HLL_vs_Rusanov_for_400_cells_of_Density.png)
-![HLL_vs_Rusanov_for_800_cells_of_Density.png](./HLL_vs_Rusanov_for_800_cells_of_Density.png)
+Density vs location:
+![Result_of_Density.png](./Result_of_Density.png)
 
-Velocity of each cells:
-![HLL_vs_Rusanov_for_200_cells_of_Velocity.png](./HLL_vs_Rusanov_for_200_cells_of_Velocity.png)
-![HLL_vs_Rusanov_for_400_cells_of_Velocity.png](./HLL_vs_Rusanov_for_400_cells_of_Velocity.png)
-![HLL_vs_Rusanov_for_800_cells_of_Velocity.png](./HLL_vs_Rusanov_for_800_cells_of_Velocity.png)
+X-direction Velocity vs location:
+![Result_of_X-Dir_Velocity.png](./Result_of_X-Dir_Velocity.png)
 
-Temperature of each cells:
-![HLL_vs_Rusanov_for_200_cells_of_Temperature.png](./HLL_vs_Rusanov_for_200_cells_of_Temperature.png)
-![HLL_vs_Rusanov_for_400_cells_of_Temperature.png](./HLL_vs_Rusanov_for_400_cells_of_Temperature.png)
-![HLL_vs_Rusanov_for_800_cells_of_Temperature.png](./HLL_vs_Rusanov_for_800_cells_of_Temperature.png)
+Y-direction Velocity vs location:
+![Result_of_Y-Dir_Velocity.png](./Result_of_Y-Dir_Velocity.png)
 
-Pressure of each cells:
-![HLL_vs_Rusanov_for_200_cells_of_Pressure.png](./HLL_vs_Rusanov_for_200_cells_of_Pressure.png)
-![HLL_vs_Rusanov_for_400_cells_of_Pressure.png](./HLL_vs_Rusanov_for_400_cells_of_Pressure.png)
-![HLL_vs_Rusanovn_for_800_cells_of_Pressure.png](./HLL_vs_Rusanov_for_800_cells_of_Pressure.png)
+Temperature vs location:
+![Result_of_Temperature.png](./Result_of_Temperature.png)
+
+Pressure vs location:
+![Result_of_Pressure.png](./Result_of_Pressure.png)
--- HLL_2D_Euler/memory.c
+++ HLL_2D_Euler/memory.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void Allocate_memory(float **array1, float **array2, float **array3, float **array4, float **array5, float **array6,
-		     float **array7, float **array8, float **array9, float **array10,float **array11, int N_CELLS){
+void Allocate_memory(float **array1, float **array2, float **array3, float **array4, float **array5, float **array6, float **array7, float **array8, float **array9,
+		     float **array10, float **array11, float **array12, float **array13, float **array14, float **array15, float **array16, float **array17, float **array18,
+		     float **array19, int N_CELLS){
 	*array1 = (float*)malloc(N_CELLS * sizeof(float));
 	*array2 = (float*)malloc(N_CELLS * sizeof(float));
 	*array3 = (float*)malloc(N_CELLS * sizeof(float));
@@ -11,19 +12,29 @@ void Allocate_memory(float **array1, float **array2, float **array3, float **arr
 	*array6 = (float*)malloc(N_CELLS * sizeof(float));
 	*array7 = (float*)malloc(N_CELLS * sizeof(float));
 	*array8 = (float*)malloc(N_CELLS * sizeof(float));
-	*array9 = (float*)malloc((N_CELLS+1) * sizeof(float));
-	*array10 = (float*)malloc((N_CELLS+1) * sizeof(float));
-	*array11 = (float*)malloc((N_CELLS+1) * sizeof(float));
-	if (*array1 == NULL || *array2 == NULL || *array3 == NULL || *array4 == NULL || *array5 == NULL || *array6 == NULL ||
-	    *array7 == NULL || *array8 == NULL || *array9 == NULL || *array10 == NULL || *array11 == NULL){
+	*array9 = (float*)malloc(N_CELLS * sizeof(float));
+	*array10 = (float*)malloc(N_CELLS * sizeof(float));
+	*array11 = (float*)malloc(N_CELLS * sizeof(float));
+	*array12 = (float*)malloc(N_CELLS * sizeof(float));
+	*array13 = (float*)malloc(N_CELLS * sizeof(float));
+	*array14 = (float*)malloc(N_CELLS * sizeof(float));
+	*array15 = (float*)malloc(N_CELLS * sizeof(float));
+	*array16 = (float*)malloc(N_CELLS * sizeof(float));
+	*array17 = (float*)malloc(N_CELLS * sizeof(float));
+	*array18 = (float*)malloc(N_CELLS * sizeof(float));
+	*array19 = (float*)malloc(N_CELLS * sizeof(float));
+	if(*array1 == NULL || *array2 == NULL || *array3 == NULL || *array4 == NULL || *array5 == NULL || *array6 == NULL || *array7 == NULL || *array8 == NULL ||
+	   *array9 == NULL || *array10 == NULL || *array11 == NULL || *array12 == NULL || *array13 == NULL|| *array14 == NULL || *array15 == NULL || *array16 == NULL ||
+	   *array17 == NULL || *array18 == NULL || *array19 == NULL){
 		printf("Memory allocation failed!\n");
 		exit(1);
 	}
-	printf("Memory allocation successfully for %d elements!\n", N_CELLS);
+		printf("Memory allocation successfully for %d elements!\n", N_CELLS);
 }
 
-void Free_memory(float **array1, float **array2, float **array3, float **array4, float **array5, float **array6,
-		 float **array7, float **array8, float **array9, float **array10, float **array11){
+void Free_memory(float **array1, float **array2, float **array3, float **array4, float **array5, float **array6, float **array7, float **array8, float **array9,
+		 float **array10, float **array11, float **array12, float **array13, float **array14, float **array15, float **array16, float **array17, float **array18,
+		 float **array19){
 	free(*array1);
 	free(*array2);
 	free(*array3);
@@ -35,5 +46,13 @@ void Free_memory(float **array1, float **array2, float **array3, float **array4,
 	free(*array9);
 	free(*array10);
 	free(*array11);
-	printf("Memory freed successfully!\n");
+	free(*array12);
+	free(*array13);
+	free(*array14);
+	free(*array15);
+	free(*array16);
+	free(*array17);
+	free(*array18);
+	free(*array19);
+	printf("Memory freed successfully!\n");\
 }
