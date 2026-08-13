ff0129c3a7e5b8a915ee85bdd712bd5712405da0
JunYi <nicejunyi@gmail.com>
2026-08-13 15:10:12 +0800

    Update Calc_variable.c, INDEX = i * (NY+4) + j

--- HLL_2D_2nd_Euler/Calc_variable.c
+++ HLL_2D_2nd_Euler/Calc_variable.c
@@ -3,7 +3,7 @@
 void Calc_variable(float *p0, float *p1, float *p2, float *p3, float *p4, float *mass, float *momentum_X, float *momentum_Y, float *energy, float GAMMA, float R, int NX, int NY){
 	for (int i = 2; i < NX + 2; i++){
 		for (int j = 2; j < NY + 2; j++){
-			int INDEX = i * (NY + 2) + j;
+			int INDEX = i * (NY + 4) + j;
 			p0[INDEX] = mass[INDEX];
 			p1[INDEX] = momentum_X[INDEX] / mass[INDEX];
 			p2[INDEX] = momentum_Y[INDEX] / mass[INDEX];
