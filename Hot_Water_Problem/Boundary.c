f3af771fbbb7bb6b601b94db479a14d7df2e97ae
JunYi <nicejunyi@gmail.com>
2026-09-14 15:01:18 +0800

    Update boundary condition for calculate mass fraction

--- Hot_Water_Problem/Boundary.c
+++ Hot_Water_Problem/Boundary.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 void Boundary(float *p0, float *p1, float *p2, float *p3, float *p4, float *p5, float Ru, float R_v, float R_dry, int NX, int NY){
+	float P_atm = 101325;  // 總環境壓力
 	// LEFT and RIGHT
 	for (int j = 2 ; j <= NY+1; j++){
 		int LEFT_LEFT_GHOST = 0 * (NY+4) + j;
@@ -30,7 +31,7 @@ void Boundary(float *p0, float *p1, float *p2, float *p3, float *p4, float *p5, 
 		p3[RIGHT_GHOST] = p3[RIGHT_INNER];
 		p3[RIGHT_RIGHT_GHOST] = p3[RIGHT_INNER];
 
-		p4[LEFT_GHOST] = 101325.0; // Inflow P = 1 atm = 101325 Pa (Kg/m-s^2)
+		p4[LEFT_GHOST] = P_atm; // Inflow P = 1 atm = 101325 Pa (Kg/m-s^2)
 		p4[LEFT_LEFT_GHOST] = p4[LEFT_GHOST];
 		p4[RIGHT_GHOST] = p4[RIGHT_INNER];
 		p4[RIGHT_RIGHT_GHOST] = p4[RIGHT_INNER];
@@ -49,6 +50,18 @@ void Boundary(float *p0, float *p1, float *p2, float *p3, float *p4, float *p5, 
 		p0[RIGHT_GHOST] = p4[RIGHT_GHOST] / (R_mix_R * p3[RIGHT_GHOST]);
 		p0[RIGHT_RIGHT_GHOST] = p4[RIGHT_RIGHT_GHOST] / (R_mix_RR * p3[RIGHT_RIGHT_GHOST]);
 	}
+
+	float T_base = 373.15;  // The bottom base is maintained at 373.15 K
+	float Phi_base = 1.0;  // Relative humidtiy at bottom (100%)
+
+	float T_C = T_base - 273.15; // Back to Celsius degree
+	float P_sat = 610.78 * exp((17.27 * T_C) / (T_C + 237.3)); // Tetens equation
+	float P_v = Phi_base * P_sat;
+	if (P_v > P_atm) {
+		P_v = P_atm;
+	} // 防呆機制：水氣分壓不能超過總壓
+	float Y_base = (P_v / R_v) / ((P_v / R_v) + ((P_atm - P_v) / R_dry));
+
 	//BOTTOM and TOP
 	for (int i = 2 ; i <= NX+1; i++){
 		int BOTTOM_BOTTOM_GHOST = i * (NY+4) + 0;
@@ -68,15 +81,16 @@ void Boundary(float *p0, float *p1, float *p2, float *p3, float *p4, float *p5, 
 		p2[BOTTOM_GHOST] = -p2[BOTTOM_INNER];
 		p2[BOTTOM_BOTTOM_GHOST] = -p2[BOTTOM_BOTTOM_INNER];
 
-		float T_base = 373.15; // The bottom base is maintained at 373.15 K
 		p3[BOTTOM_GHOST] = 2 * T_base - p3[BOTTOM_INNER];
 		p3[BOTTOM_BOTTOM_GHOST] = 2 * T_base - p3[BOTTOM_BOTTOM_INNER];
 
 		p4[BOTTOM_GHOST] = p4[BOTTOM_INNER];
 		p4[BOTTOM_BOTTOM_GHOST] = p4[BOTTOM_BOTTOM_INNER];
 
-		p5[BOTTOM_GHOST] = 1.0; // The max relative humidity (phi_max) setting 1
-		p5[BOTTOM_BOTTOM_GHOST] = p5[BOTTOM_GHOST];
+		p5[BOTTOM_GHOST] = fmax(0.0, fmin(2 * Y_base - p5[BOTTOM_INNER], 1.0));
+		p5[BOTTOM_BOTTOM_GHOST] = fmax(0.0, fmin(2 * Y_base - p5[BOTTOM_BOTTOM_INNER], 1.0));
+//		p5[BOTTOM_GHOST] = 1.0; // The max relative humidity (phi_max) setting 1
+//		p5[BOTTOM_BOTTOM_GHOST] = p5[BOTTOM_GHOST];
 
 		// rho = P / RT
 		float R_mix_B = (1 - p5[BOTTOM_GHOST]) * R_dry + p5[BOTTOM_GHOST] * R_v;
@@ -94,8 +108,8 @@ void Boundary(float *p0, float *p1, float *p2, float *p3, float *p4, float *p5, 
 		p3[TOP_GHOST] = p3[TOP_INNER];
 		p3[TOP_TOP_GHOST] = p3[TOP_INNER];
 
-		p4[TOP_GHOST] = p4[TOP_INNER];
-		p4[TOP_TOP_GHOST] = p4[TOP_INNER];
+		p4[TOP_GHOST] = P_atm;
+		p4[TOP_TOP_GHOST] = P_atm;
 
 		p5[TOP_GHOST] = p5[TOP_INNER];
 		p5[TOP_TOP_GHOST] = p5[TOP_INNER];
